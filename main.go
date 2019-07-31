cdd78002f260ae1cd61216ca01b26c467d0e6d46
EunsuChoi76 <53501218+EunsuChoi76@users.noreply.github.com>
2019-07-31 13:00:24 +0900

    CES2

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World CES"}
+		res := &response{Message: "Hello World CES2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
